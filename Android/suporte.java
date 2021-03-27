fe3df29db9fb06a2fcbd546fa1b19e30c4430b2b
GuilhermeGCS <guilhermegcs222@gmail.com>
2021-03-27 08:39:22 -0300

    First commit

---
+++ Android/suporte.java
@@ -0,0 +1,9 @@
+/*
+ * Classname             (GuiGCS-BOT)
+ *
+ * Version information   (3.0)
+ *
+ * Date                  (06/12,15:35)
+ *
+ * author                (GuilhermeGCS)
+ * Copyright notice      (bot com suporte a Android/Java)
---
+++ interface/name.php
@@ -0,0 +1,3 @@
+___| Ferramenta By __|
+|  GuiGCS__|
+|__________|
